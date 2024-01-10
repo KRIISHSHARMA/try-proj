e254bf739945aea15ff26e8a5e5770c56339eec8
Kriish sharma <kriish.sharma2006@gmail.com>
2024-01-10 13:25:33 +0530

    Update script.js

--- script.js
+++ script.js
@@ -12,7 +12,7 @@ const material = new THREE.MeshBasicMaterial( { color: 0x00fb00 } );
 const cube = new THREE.Mesh( geometry, material );
 scene.add( cube );
 
-camera.position.z = 2;
+camera.position.z = 5;
 
 function animate() {
 	requestAnimationFrame( animate );
